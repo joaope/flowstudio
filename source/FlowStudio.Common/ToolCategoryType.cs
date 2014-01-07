3040b017a4455a8600e1968d573c0ebf33123981
João Correia <joaope@gmail.com>
2014-01-07 16:32:14 +0000

    Changed url on header infos

--- source/FlowStudio.Common/ToolCategoryType.cs
+++ source/FlowStudio.Common/ToolCategoryType.cs
@@ -1,6 +1,6 @@
 ﻿// Copyright (C) 2013 João Pedro Correia - All Rights Reserved
 // Code under the terms of the GPLv2 license
-// URL: http://flowstudio.codeplex.com
+// URL: https://github.com/JoaoPe/flowstudio
 
 namespace FlowStudio.Common
 {
